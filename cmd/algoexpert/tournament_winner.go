bb24a50083f99002c3657cef4c022df6d6363c6c
kaique <caires.kaique@gmail.com>
2024-09-06 05:53:30 -0300

    feat: added tournament winner algorithm

---
+++ cmd/algoexpert/tournament_winner.go
@@ -0,0 +1,30 @@
+package algoexpert
+
+const HOME_TEAM_WON = 1
+
+type TeamWithHighestScore struct {
+	Team  string
+	Score int
+}
+
+func TournamentWinner(competitions [][]string, results []int) string {
+	scores := map[string]int{}
+	teamWithHighestScore := &TeamWithHighestScore{}
+
+	for i, competition := range competitions {
+		winnerIdx := results[i] ^ 1
+
+		if _, ok := scores[competition[winnerIdx]]; ok {
+			scores[competition[winnerIdx]] += 3
+		} else {
+			scores[competition[winnerIdx]] = 3
+		}
+
+		if teamWithHighestScore.Score < scores[competition[winnerIdx]] {
+			teamWithHighestScore.Team = competition[winnerIdx]
+			teamWithHighestScore.Score = scores[competition[winnerIdx]]
+		}
+	}
+
+	return teamWithHighestScore.Team
+}
