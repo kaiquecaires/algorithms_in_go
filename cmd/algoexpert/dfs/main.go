ede9b50496caa10a8ec7259998cf8648d5756422
kaique <caires.kaique@gmail.com>
2024-09-07 06:51:56 -0300

    feat: added dfs

---
+++ cmd/algoexpert/dfs/main.go
@@ -0,0 +1,14 @@
+package dfs
+
+type Node struct {
+	Name     string
+	Children []*Node
+}
+
+func (n *Node) DepthFirstSearch(array []string) []string {
+	array = append(array, n.Name)
+	for _, node := range n.Children {
+		array = node.DepthFirstSearch(array)
+	}
+	return array
+}
