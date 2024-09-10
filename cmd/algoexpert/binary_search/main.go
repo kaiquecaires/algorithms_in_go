a4e0b9a29dda8e481773de36f33259841e8a4825
kaique <caires.kaique@gmail.com>
2024-09-10 05:51:55 -0300

    feat: added binary search

---
+++ cmd/algoexpert/binary_search/main.go
@@ -0,0 +1,19 @@
+package binarysearch
+
+func BinarySearch(array []int, target int) int {
+	l, r := 0, len(array)-1
+
+	for l <= r {
+		m := (l + r) / 2
+
+		if array[m] == target {
+			return m
+		} else if array[m] < target {
+			l = m + 1
+		} else if array[m] > target {
+			r = m - 1
+		}
+	}
+
+	return -1
+}
