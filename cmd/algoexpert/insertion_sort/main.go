f8a09b00badb3a2043fa9f9055679c4dcbfd39dd
kaique <caires.kaique@gmail.com>
2024-09-11 06:52:00 -0300

    feat: added insertion sort

---
+++ cmd/algoexpert/insertion_sort/main.go
@@ -0,0 +1,15 @@
+package insertionsort
+
+func InsertionSort(array []int) []int {
+	for i := 1; i < len(array); i++ {
+		for j := i; j > 0; j-- {
+			if array[j] < array[j-1] {
+				array[j], array[j-1] = array[j-1], array[j]
+			} else {
+				break
+			}
+		}
+	}
+
+	return array
+}
